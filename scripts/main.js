cbc23bf4032b9bb858252285ce0f859b6e6f7d31
Vinson2004 <71330519+Vinson2004@users.noreply.github.com>
2021-03-02 17:05:25 -0500

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,6 @@
+
+function welcome() {
+    let username = prompt("What is your name?");    
+    let message = "Welcome, " + username;
+    document.getElementById('saywelcome').innerHTML = message; 
+}
