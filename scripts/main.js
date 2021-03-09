e3bb11ad2a2914fde76636d70f845500658925d5
Vinson2004 <71330519+Vinson2004@users.noreply.github.com>
2021-03-08 21:24:49 -0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -4,3 +4,66 @@ function welcome() {
     let message = "Welcome, " + username;
     document.getElementById('saywelcome').innerHTML = message; 
 }
+
+function f1(){
+
+     var change = document.getElementById("button1");
+     change.innerHTML = 'Coming Soon';
+    
+}
+   
+ function f2(){
+       
+     var change = document.getElementById("button2");
+     change.innerHTML = 'Coming Soon';
+    
+}
+   
+function f3(){
+       
+     var change = document.getElementById("button3");
+     change.innerHTML = 'Coming Soon';
+       
+}
+   
+function f4(){
+       
+     var change = document.getElementById("button4");
+     change.innerHTML = 'Coming Soon';
+       
+}
+   
+function f5(){
+       
+     var change = document.getElementById("button5");
+     change.innerHTML = 'Coming Soon';
+       
+}
+   
+function f6(){
+       
+     var change = document.getElementById("button6");
+     change.innerHTML = 'Coming Soon';
+       
+}
+   
+function f7(){
+       
+     var change = document.getElementById("button7");
+     change.innerHTML = 'Coming Soon';
+       
+}
+   
+function f8(){
+       
+     var change = document.getElementById("button8");
+     change.innerHTML = 'Coming Soon';
+       
+}
+   
+function f9(){
+       
+     var change = document.getElementById("button9");
+     change.innerHTML = 'Coming Soon';
+       
+}
