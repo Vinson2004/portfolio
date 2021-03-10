0cea5be80b43c22bd4ab86d048527d83cddb9936
Vinson2004 <71330519+Vinson2004@users.noreply.github.com>
2021-03-09 19:35:25 -0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -5,65 +5,65 @@ function welcome() {
     document.getElementById('saywelcome').innerHTML = message; 
 }
 
-function f1(){
+ function f1(){
 
      var change = document.getElementById("button1");
-     change.innerHTML = 'Coming Soon';
-    
+     change.innerHTML = 'Project 1 Coming Soon...';
+     
 }
-   
+    
  function f2(){
-       
+        
      var change = document.getElementById("button2");
-     change.innerHTML = 'Coming Soon';
+     change.innerHTML = 'Project 2 Coming Soon...';
+     
+}
     
-}
-   
-function f3(){
-       
+ function f3(){
+        
      var change = document.getElementById("button3");
-     change.innerHTML = 'Coming Soon';
-       
+     change.innerHTML = 'Project 3 Coming Soon...';
+        
 }
-   
+    
 function f4(){
-       
+        
      var change = document.getElementById("button4");
-     change.innerHTML = 'Coming Soon';
-       
+     change.innerHTML = 'Project 4 Coming Soon...';
+        
 }
-   
+    
 function f5(){
-       
+        
      var change = document.getElementById("button5");
-     change.innerHTML = 'Coming Soon';
-       
+     change.innerHTML = 'Project 5 Coming Soon...';
+        
 }
-   
+    
 function f6(){
-       
-     var change = document.getElementById("button6");
-     change.innerHTML = 'Coming Soon';
-       
+        
+    var change = document.getElementById("button6");
+    change.innerHTML = 'Project 6 Coming Soon...';
+        
 }
-   
+    
 function f7(){
-       
-     var change = document.getElementById("button7");
-     change.innerHTML = 'Coming Soon';
-       
+        
+    var change = document.getElementById("button7");
+    change.innerHTML = 'Project 7 Coming Soon...';
+        
 }
-   
+    
 function f8(){
-       
-     var change = document.getElementById("button8");
-     change.innerHTML = 'Coming Soon';
-       
+        
+    var change = document.getElementById("button8");
+    change.innerHTML = 'Project 8 Coming Soon...';
+        
 }
-   
+    
 function f9(){
-       
-     var change = document.getElementById("button9");
-     change.innerHTML = 'Coming Soon';
-       
+        
+    var change = document.getElementById("button9");
+    change.innerHTML = 'Project 9 Coming Soon...';
+        
 }
